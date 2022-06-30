a69598fb7c7f2222a4720e3bfd30c714989ae9c5
Haena <kimhn200605@gmail.com>
2022-07-01 05:32:45 +0900

    [Lv1] 비밀 지도.py

---
+++ Kakao/비밀 지도.py
@@ -0,0 +1,32 @@
+def solution(n, arr1, arr2):
+    answer = []
+    n_arr1 = []
+    n_arr2 = []
+    
+    for i in range(n) :
+        b = bin(arr1[i])[2:]
+        while (True) :
+            if len(b) == n :
+                break
+            else :
+                b = '0' + b
+        n_arr1.append(b)
+        
+    for i in range(n) :
+        b = bin(arr2[i])[2:]
+        while (True) :
+            if len(b) == n :
+                break
+            else :
+                b = '0' + b
+        n_arr2.append(b)
+    
+    for i in range(n) :
+        temp = ''
+        for k in range(n) :
+            if (n_arr1[i][k] == n_arr2[i][k]) and n_arr1[i][k] == "0" :
+                temp += ' '
+            else :
+                temp += '#'
+        answer.append(temp)
+    return answer
