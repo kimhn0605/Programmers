6d13d056d2c31eac812d685edd76c076c077318b
Haena <kimhn200605@gmail.com>
2022-07-03 15:37:48 +0900

    [Lv1] 예산.py

---
+++ Lv1/예산.py
@@ -0,0 +1,13 @@
+def solution(d, budget):
+    answer = 0
+    d.sort()
+    cnt = 0
+    now = 0
+    
+    for i in d :
+        if now + i > budget :
+            break
+        else :
+            cnt += 1
+            now += i
+    return cnt
