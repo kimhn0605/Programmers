6f23bff58474c28efbe72aab09c3ca905e1b0497
Haena <kimhn200605@gmail.com>
2022-09-07 12:07:12 +0900

    [Lv1] 성격 유형 검사하기.py

---
+++ Lv1/성격 유형 검사하기.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+alpha = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']]
+li = defaultdict(int)
+
+def solution(survey, choices):
+    answer = ''
+    for i in range(len(survey)) :
+        if choices[i] < 4 :
+            li[survey[i][0]] += (4-choices[i])
+        elif choices[i] > 4 :
+            li[survey[i][1]] += (choices[i]-4)
+    for al in alpha :
+        answer += comp(al[0], al[1])
+    return answer
+
+def comp(f, s):
+    if li[f] == li[s] :
+        return f
+    elif li[f] < li[s] :
+        return s
+    else :
+        return sorted([f, s])[0]
+    
